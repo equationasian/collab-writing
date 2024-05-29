494e835c3d62abe25d1e714d75e92ebc35fef3fd
Anna Nguyen <1110nguyena@gmail.com>
2024-05-29 17:39:07 -0500

    Initial commit

---
+++ backend/src/main/java/com/example/collab_writing/CollabWritingApplication.java
@@ -0,0 +1,13 @@
+package com.example.collab_writing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabWritingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabWritingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/collab_writing/CollabWritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collab_writing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabWritingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
